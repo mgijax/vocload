4c8382f2d370df84276035a588e1566367ed1451
tcw <>
2002-02-25 16:40:03 +0000

    Added some comments

--- html.py
+++ html.py
@@ -10,24 +10,31 @@ import vocloadlib
 EOL = '\n'
 TODAYS_DATE = vocloadlib.timestamp("")
 
+# Starts an html table
 def getStartTableHTML ():
    return "<Table border cellpadding = 2 cellspacing = 3 width = 90%><TR align=center>" + EOL
 
+# Ends an html table
 def getEndTableHTML():
    return "</Table>" + EOL
 
+# Writes a table cell
 def getCellHTML ( cellContent ):
    return "<TD>" + cellContent + "</TD>" + EOL
 
+# Writes a table column header
 def getTableHeaderLabelHTML( cellContent ):
    return getCellHTML ("<B>" + cellContent + "</B>") + EOL
 
+# Starts a table row
 def getStartTableRowHTML():
    return "<TR align=center>" + EOL
 
+# Ends a table row
 def getEndTableRowHTML():
    return "</TR>" + EOL
 
+# Starts an HTML document
 def getStartHTMLDocumentHTML( documentTitle ):
    return "<HTML>" + EOL + \
           "<HEAD>" + EOL + \
@@ -37,6 +44,7 @@ def getStartHTMLDocumentHTML( documentTitle ):
           "<H1><U><CENTER>" + documentTitle + "</H1></U></CENTER>" + EOL + \
           "<H2><CENTER><I> DATE:" + TODAYS_DATE + "</I></CENTER></H2>"
 
+# Commits an HTML document
 def getEndHTMLDocumentHTML():
    return "</BODY>" + EOL + \
           "</HTML>" + EOL
