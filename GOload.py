64c25430eaef3276446096717675da82f1f1b356
Lori Corbani <Lori.Corbani@jax.org>
2003-03-25 15:28:33 +0000

    renamed from GO.load

---
+++ GOload.py
@@ -0,0 +1,129 @@
+#!/usr/local/bin/python
+
+'''
+#
+# History
+#
+# 08/26/2002 lec
+#	- TR 3809 (adding support for processing Phenotype vocabulary in GO format)
+#	- added call to govocab.initializeRegExps (self.config.getConstant('ACC_PREFIX'))
+#	- modified setID() to : self.name = self.config.getConstant('VOCAB_NAME')
+#
+'''
+
+import sys
+import os
+import string
+
+import vocloadlib
+import loadWrapper
+import tempfile
+import GOVocab
+
+d = os.environ("DEFS_FILE")
+DEFS_FILE = os.environ("DEFS_FILE")
+TERM_FILE = os.environ("TERM_FILE")
+VOCAB_NAME = os.environ("VOCAB_NAME")
+ACC_PREFIX = os.environ("ACC_PREFIX")
+
+def textEdgeType (char):
+        if char == '%':
+                return 'is-a'
+        else:                   # char == '<'
+                return 'part-of'
+
+class GO_Wrapper (loadWrapper.LoadWrapper):
+        USAGE = '''%s [-f|-i][-n][-l <file>] <RcdFile>
+        -f | -i : full load or incremental load? (default is full)
+        -n      : use no-load option (log changes, but don't execute them)
+        -l      : name of the log file to create (default is stderr only)
+        RcdFile : name of the RcdFile-formatted configuration file to read
+''' % sys.argv[0]
+
+        NUM_ARGS = 1
+
+        def preProcess (self):
+                defs_file = DEFS_FILE
+
+                term_fp = open (TERM_FILE, 'w')
+
+                for (key, dag) in self.config.items():
+                        self.log.writeline ('Pre-processing %s to create %s' \
+                                % (dag['ONTOLOGY_FILE'], dag['LOAD_FILE']))
+
+                        govocab = GOVocab.GOVocab ()
+			govocab.initializeRegExps (ACC_PREFIX)
+                        govocab.buildVocab (defs_file, dag['ONTOLOGY_FILE'])
+
+                        dag_fp = open (dag['LOAD_FILE'], 'w')
+
+                        # [ (parent, [ (child, edge type), ... ]), ... ]
+
+                        to_do = [ ('', [(govocab.getRoot(), '') ] ) ]
+
+                        done = {}       # acc ID -> 1 if added already
+
+                        obsolete = {}   # primary IDs of obs. terms
+
+                        while to_do:
+                                parent, children = to_do[0]
+                                del to_do[0]
+
+                                for child, edge_type in children:
+                                    childID = child.getId()
+
+                                    # a child is obsolete if its parent is
+                                    # obsolete or if its label is 'obsolete'.
+                                    # otherwise, it's current.
+
+                                    if parent and obsolete.has_key(parent):
+                                        status = 'obsolete'
+                                        obsolete[childID] = 1
+                                    elif child.getLabel() == 'obsolete':
+                                        status = 'obsolete'
+                                        obsolete[childID] = 1
+                                    else:
+                                        status = 'current'
+
+                                    if not done.has_key (childID):
+                                        term_fp.write (loadWrapper.TERM_LINE \
+                                            % (child.getLabel(),
+                                                childID,
+                                                status,
+                                                '',             # abbrev
+                                                child.getDefinition(),
+                                                string.join (
+                                                        child.getSynonyms(),
+                                                        '|'),
+                                                #'' ))
+                                                string.join (
+                                                        child.getSecondaryGOIDs(), 
+                                                        '|' ))) # 2ndary IDs
+                                        done[childID] = 1
+
+                                        to_do.append ( (childID,
+                                                govocab.getChildrenOf(child))
+                                                )
+                                    if parent:
+                                        dag_fp.write (loadWrapper.DAG_LINE % \
+                                                (childID, '',
+                                                textEdgeType (edge_type),
+                                                parent)
+                                                )
+                                    else:
+                                        # child is parent-less (root) node:
+
+                                        dag_fp.write (loadWrapper.DAG_LINE % \
+                                                (childID, '', '', ''))
+                        dag_fp.close()
+
+                term_fp.close()
+                return
+
+        def setID (self):
+                self.name = VOCAB_NAME
+                return
+
+if __name__ == '__main__':
+        wrapper = GO_Wrapper (sys.argv[1:])
+        wrapper.go()
