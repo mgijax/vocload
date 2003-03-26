b05360f7fc4c5ab6b5b72950afa11971fa8392f4
Lori Corbani <Lori.Corbani@jax.org>
2003-03-26 13:19:21 +0000

    TR 3853

--- runOMIMLoad.sh
+++ runOMIMLoad.sh
@@ -1,52 +1,27 @@
 #!/bin/sh
 
 #
-# Main Wrapper Script for Downloading OMIM files and loading OMIM terms
+# Program: runOMIMLoad.sh
 #
-
-# Define return codes
-SUCCESS=0
-FAILURE=1
-
-# These environment variables should really be placed inside the omim.rcd; however,
-# since the omim.rcd is python-based they are temporarily placed here.  To address
-# this problem in the future, we should write a python program which reads
-# and exports all environment variables in an .rcd file, placing the call
-# to the program inside the shell script. For now, the variables are placed here.
-RUNTIME_DIR="./runTimeOMIM/"
-ARCHIVE_DIR="./archiveOMIM/"
-
-SYBASE=/opt/sybase/12.5
-PYTHONPATH=/usr/local/mgi/lib/python
-PATH=$PATH:.:/usr/bin:$SYBASE/ASE-12_5/bin:$SYBASE/OCS-12_5/bin:/usr/java/bin
-FULL_LOG_FILE=$RUNTIME_DIR"fullLog.txt"
-MAINTAINER="lec@informatics.jax.org"
-MAIL_FILE_NAME=$RUNTIME_DIR"mail.txt"
-ARCHIVE_FILE_NAME=$ARCHIVE_DIR"vocload.`date +%Y%m%d:%H:%M`.jar"
-LOAD_LOG_FILE=$RUNTIME_DIR"log.txt"
-LOAD_DATA_FILE="omim.tab"
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SYBASE/OCS-12_5/lib
-
-export RUNTIME_DIR
-export SYBASE
-export PYTHONPATH
-export PATH
-export LD_LIBRARY_PATH
+# Purpose:
+#
+# Main Wrapper Script for Downloading Ontology files and generating OMIM Terms
+# 
+# Usage:
+#
+#	runOMIMLoad.sh [load|noload] [full|incremental]
+#
+# History:
+#
+#	lec	03/25/2003
+#	- use new Configuration and OMIM.config files
+#
 
 die()
 {
    echo $1
    cat $FULL_LOG_FILE | mailx -s "OMIM Load Catastrophic FAILURE" $MAINTAINER 
    exit $FAILURE
-}
-
-changeToRunDirectory()
-{
-   APPDIR=`dirname $0`
-   if test $APPDIR != "."
-   then
-      cd "$APPDIR"
-   fi
 }
 
 createDir()
@@ -77,8 +52,9 @@ writePgmLogFile()
 
 JOB_SUCCESSFUL="false"
 
-changeToRunDirectory
-umask 002
+# change to directory where this file resides
+cd `dirname $0`
+. OMIM.config
 createDir $RUNTIME_DIR
 createDir $ARCHIVE_DIR
 echo "Job Started: `date`"
@@ -113,39 +89,39 @@ echo  "MODE STATUS is $2" >> $FULL_LOG_FILE 2>&1
 echo "*****************************************" >> $FULL_LOG_FILE 2>&1
 
 ######################################################
-# 1. Run omim.load program
+# 1. Run GOload.py program
 ######################################################
-LOAD_PROGRAM=omim.load
-LOAD_PROGRAM_CALL="omim.load $LOAD_FLAG $MODE_FLAG -l $LOAD_LOG_FILE omim.rcd $LOAD_DATA_FILE"
+MP_LOAD_PROGRAM=GOload.py
+MP_LOAD_PROGRAM_CALL="${MP_LOAD_PROGRAM} $LOAD_FLAG $MODE_FLAG -l $MP_LOAD_LOG_FILE ${RCD_FILE}"
 
-writePgmExecutionHeaders $LOAD_PROGRAM
-echo $LOAD_PROGRAM_CALL                       >> $FULL_LOG_FILE 2>&1
+writePgmExecutionHeaders $MP_LOAD_PROGRAM
+echo $MP_LOAD_PROGRAM_CALL                       >> $FULL_LOG_FILE 2>&1
 echo "*****************************************" >> $FULL_LOG_FILE 2>&1
 
-msg=`$LOAD_PROGRAM_CALL 2>&1`
+msg=`$MP_LOAD_PROGRAM_CALL 2>&1`
 rc=$?
 
-writePgmLogFile $LOAD_PROGRAM, $LOAD_LOG_FILE
+writePgmLogFile $MP_LOAD_PROGRAM, $MP_LOAD_LOG_FILE
 
 case $rc in
      $FAILURE)
-        ERROR_MSG="omim.load FAILED!!!! - Check Log File: $FULL_LOG_FILE"
+        ERROR_MSG="${MP_LOAD_PROGRAM} FAILED!!!! - Check Log File: $FULL_LOG_FILE"
         echo $ERROR_MSG
         echo $0:$ERROR_MSG                 >> $FULL_LOG_FILE 2>&1
-        echo "$0:Ouput is: $msg" >> $FULL_LOG_FILE 2>&1
+        echo "$0:${MP_LOAD_PROGRAM} Ouput is: $msg" >> $FULL_LOG_FILE 2>&1
         die "$ERROR_MSG";;
 
      $SUCCESS)
-        ERROR_MSG="omim.load Was Successful - No Errors Encountered"
+        ERROR_MSG="${MP_LOAD_PROGRAM} Was Successful - No Errors Encountered"
         JOB_SUCCESSFUL="true"
         echo $ERROR_MSG
         echo $0:$ERROR_MSG                 >> $FULL_LOG_FILE 2>&1;;
 esac
-cat $LOAD_LOG_FILE                      >> $FULL_LOG_FILE 2>&1
-LOAD_ERROR_MSG=$ERROR_MSG
+cat $MP_LOAD_LOG_FILE                      >> $FULL_LOG_FILE 2>&1
+MP_LOAD_ERROR_MSG=$ERROR_MSG
 
 ######################################################
-# 3. Finally, archive the files
+# 2. Finally, archive the files
 ######################################################
 # We are using "jar" rather than "tar", because jar compressed
 # the files to 6 times smaller than tar and its syntax is 
@@ -196,7 +172,7 @@ echo $SUBJECT
 
 echo "Run Summary:"                                                                  > $MAIL_FILE_NAME
 echo "****************************************************************************" >> $MAIL_FILE_NAME
-echo "OMIM Load Program Completion Status: $LOAD_ERROR_MSG"                         >> $MAIL_FILE_NAME
+echo "OMIM Load Program Completion Status: $OMIM_LOAD_ERROR_MSG"                    >> $MAIL_FILE_NAME
 echo "****************************************************************************" >> $MAIL_FILE_NAME
 echo "Archive Program Completion Status: $JAR_PROGRAM_ERROR_MSG"                    >> $MAIL_FILE_NAME
 echo "****************************************************************************" >> $MAIL_FILE_NAME
@@ -212,4 +188,6 @@ echo "Job Complete: `date`"
 echo "Job Complete: `date`"                                                         >> $FULL_LOG_FILE 2>&1
 
 cat $MAIL_FILE_NAME $FULL_LOG_FILE | mailx -s "$SUBJECT" $MAINTAINER 
+
+# $Log$
 
