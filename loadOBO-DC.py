4aa8229719cc1279b93761b2b701b2cebc6c8a25
MGI Admin <mgiadmin@jax.org>
2013-10-04 17:19:37 +0000

    TR11423/HDP

--- loadOBO-DC.py
+++ loadOBO-DC.py
@@ -47,7 +47,7 @@ def createSynonymFile():
 	# translate the DC-OBO file into a loadSynonym-input file
 	#
 
-	global omimList
+	global omimList, omimAllList
 
 	outFile = open(os.environ['DCLUSTERSYN_FILE'], 'w')
 
@@ -75,7 +75,6 @@ def createSynonymFile():
 				continue
 
 			outFile.write(omimID + '\tdisease cluster\t' + synonym + '\n')
-
 			omimList.add((omimID, synonym))
 
 	outFile.close()
@@ -83,13 +82,16 @@ def createSynonymFile():
 
 def createDiffFile():
 	#
-	# check if the omim ID is in MGD but not in the cluster file
-	# a one-time-only report?
+	# 1) OMIM terms that exist in MGI but do *not* exists in the OBO-DC file
+	# 2) OMIM terms that exist in the OBO-DC file but do *not* exist in MGI
 	#
 
 	global omimAllList
 
-	outFile = open(os.environ['DCLUSTERALL_FILE'], 'w')
+	mgiList = {}
+
+	outFile1 = open(os.environ['DCLUSTERDIFF1_FILE'], 'w')
+	outFile2 = open(os.environ['DCLUSTERDIFF2_FILE'], 'w')
 
 	results = db.sql('''
 		select a.accID, t.term
@@ -102,14 +104,22 @@ def createDiffFile():
 		''', 'auto')
 
 	for r in results:
-		# write file in OBO-format
-		if r['accID'] not in omimAllList:
-			outFile.write('[Term]\n')
-			outFile.write('id: OMIM:' + r['accID'] + '\n')
-			outFile.write('name: ' + r['term'] + '\n')
-			outFile.write('is_a: DC:0000138 ! Disease Cluster\n\n')
+		mgiList.setdefault(r['accID'], []).append(r['term'])
 
-	outFile.close()
+	for accID in mgiList:
+		if accID not in omimAllList:
+			# write file in OBO-format
+			outFile1.write('[Term]\n')
+			outFile1.write('id: OMIM:' + accID + '\n')
+			outFile1.write('name: ' + mgiList[accID][0] + '\n')
+			outFile1.write('is_a: DC:0000138 ! Disease Cluster\n\n')
+
+	#for r in omimAllList:
+		#if r not in mgiList:
+
+	outFile1.close()
+	outFile2.close()
+
 	return 0
 
 #
