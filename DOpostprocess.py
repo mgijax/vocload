d662f28cf1d327414b7616b01906611c4d229712
Lori Corbani <lori.corbani@jax.org>
2016-12-07 10:36:17 -0500

    TR12427/Disase Ontology (DO)

--- DOpostprocess.py
+++ DOpostprocess.py
@@ -41,80 +41,48 @@ doFileName = None
 # do file pointer
 doFile = None
 
-# do text formatted file
-doAccFileName = None
-# do text file pointer
-doAccFile = None
-
 # insert statement
 INSERT_ACCESSION = '''insert into ACC_Accession 
 	values ((select max(_Accession_key) + 1 from ACC_Accession), 
 		'%s', '%s', %s, 15, %s, 13, 0, 0)
 '''
 
-doLookup = {}
+doFileName = os.environ['OBO_FILE']
+doFile = open(doFileName, 'r')
 
-def processDO():
+omimIdValue = 'id: OMIM:'
+relValue = 'relationship: RO:0003304'
+skipValue = 'OMIM:000000'
+foundOMIM = 0
 
-    doFileName = os.environ['OBO_FILE']
-    doAccFileName = os.environ['DO_ACC_FILE']
-    doFile = open(doFileName, 'r')
-    doAccFile = open(doAccFileName, 'w')
+for line in doFile.readlines():
 
-    idValue = 'id: '
-    doIdValue = 'id: OMIM:'
-    relValue = 'relationship: RO:0003304'
-    foundOMIM = 0
+    # find [Term]
+    # find relationship: RO:0003304
 
-    for line in doFile.readlines():
+    if line == '[Term]':
+        foundOMIM = 0
 
-        # find [Term]
-        # find relationship: RO:0003304
+    elif line[:9] == omimIdValue:
+        omimId = line[4:-1]
+	if omimId == skipValue:
+	    continue
+	foundOMIM = 1
 
-        if line == '[Term]':
-	    foundOMIM = 0
+    elif foundOMIM and line[:24] == relValue:
 
-        elif line[:9] == doIdValue:
-            omimId = line[4:-1]
-	    if omimId == 'OMIM:000000':
-	        continue
-	    foundOMIM = 1
+        tokens = line[25:-1].split(' ')
+	doId = tokens[0]
 
-        elif foundOMIM and line[:24] == relValue:
+        prefixPart, numericPart = accessionlib.split_accnum(omimId)
+        objectKey = loadlib.verifyObject(doId, 13, None, None, None)
+        addSQL = INSERT_ACCESSION % (omimId, prefixPart, numericPart, objectKey)
+	print addSQL
+        db.sql(addSQL, None)
 
-            tokens = line[25:-1].split(' ')
-	    doId = tokens[0]
+    else:
+        continue
 
-            prefixPart, numericPart = accessionlib.split_accnum(omimId)
-            objectKey = loadlib.verifyObject(doId, 13, None, None, None)
-
-            addSQL = INSERT_ACCESSION % (omimId, prefixPart, numericPart, objectKey)
-	    print addSQL
-            db.sql(addSQL, None)
-
-            if doId not in doLookup:
-                doLookup[doId] = []
-            doLookup[doId].append(omimId)
-
-        else:
-            continue
-
-    for d in doLookup:
-        doAccFile.write(d + '\t' + doLookup[d][0] + '\n')
-
-    doFile.close()
-    doAccFile.close()
-
-    db.commit()
-
-    return doLookup
-
-if __name__ == '__main__':
-
-    doLookup = processDO()
-
-    print 'rows:', len(doLookup)
-
-    for d in doLookup:
-        print d, doLookup[d]
+doFile.close()
+db.commit()
 
