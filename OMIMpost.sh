aa5087ba7115b3faa200281789d129bfa3051f0f
Lori Corbani <lori.corbani@jax.org>
2020-09-28 14:21:39 -0400

    TR13349/Genome Build 39 project

---
+++ OMIMpost.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# Program: OMIMpost.sh
+#
+# Purpose:
+#
+#   Configuration file for Disease Ontology loading into MGI_Set/MGI_SetMember table
+#
+# History:
+#
+# 09/28/2020	lec
+#
+
+. ${VOCLOAD}/OMIM.config
+
+cd ${VOCLOAD}
+
+${PYTHON} OMIMtermcheck.py
+STAT=$?
+if [ ${STAT} -ne 0 ]
+then
+   exit 1
+fi
+exit 0
